f00369ab212192a7d9d93dd7dde6f29867434f71
Kale-8 <kaled.geme2@gmail.com>
2025-05-14 13:29:19 -0500

    Refactor and expand functionality of utility scripts.
    Updated book tracker to enhance input validation and return formats. Added student grade manager and restaurant menu editor modules with complete CRUD functionality to manage data effectively. Improved organization and error handling across the script.

--- 3-exercises3/functions.py
+++ 3-exercises3/functions.py
@@ -1,31 +1,79 @@
 # Utils
 import re
 
-def validation(text:str, condition, data_type,custom_alert:str='')->str or float or int:
+from cloudinit.templater import JUndefined
+
+
+def validation(value:any, condition,custom_alert:str='')->any:
     alert = lambda: print(f'\nThe number or character entered is incorrect, please enter it again.{custom_alert}')
     while True:
         try:
-            value:data_type = data_type(input(f'\n{text}'))
-            if data_type == str: value = value.lower().strip()
+            if type(value) is str: value = value.lower().strip()
             if condition(value): return value
             alert()
         except ValueError: alert()
 
 # 1. Library Book Tracker
-book_list:dict = {}
+library:dict = {}
 def add_book(title:str,author:str,pages:int)->None:
-    if title.lower().strip() in book_list:
+    if title in library:
         print("Book already exists.")
         return
-    book_list[title] = {"author":author,"pages":pages}
-def find_book(title:str)->None:
-    if title in book_list:
-        print(f"{title.title()} by {book_list[title]['author'].title()} has {book_list[title]['pages']} pages.")
-    else: print("Book not found.")
+    library[validation(title, lambda x: re.search(r'^[a-z\d\s]+$', x, re.IGNORECASE),' Only letters, numbers and spaces between words are allowed')] = \
+        {
+        "author":validation(author, lambda x: re.search(r'^[a-z\d\s]+$', x, re.IGNORECASE),' Only letters, numbers and spaces between words are allowed'),
+        "pages":validation(pages, lambda x: x >= 1,' The number of pages must be greater than 0')
+        }
+def find_book(title:str)->str:
+    if title in library:
+        return f"{title.title()} by {library[title]['author'].title()} has {library[title]['pages']} pages."
+    else:
+        return 'Book not found.'
 def show_books()->None:
-    if book_list:
+    if library:
         print(f"{'Title':<20}{'Author':^20}{'Pages':>10}")
-        for title,book in book_list.items():
+        for title,book in library.items():
+            print(f"{title.title():<20}{book['author'].title():^20}{book['pages']:>10}")
+    else: print("No books found.")
+
+#  2. Student Grade Manager
+grades:dict = {}
+def add_student(name:str)->None:
+    if name in grades:
+        print("Student already exists.")
+        return
+    grades[validation(name, lambda x: re.search(r'^[a-z\s]+$', x, re.IGNORECASE), ' Only letters and spaces between words are allowed')] = []
+def add_grade(name:str, grade)->None:
+    name = name.lower().strip()
+    if name in grades:
+        grade = validation(grade, lambda x: 0 <= x <= 100, ' The grade must be between 0 and 100')
+        grades[name].append(grade)
+    else: print("Student not found.")
+def get_average(name:str)->any:
+    name = name.lower().strip()
+    if name in grades:
+        if grades[name]: return sum(grades[name])/len(grades[name])
+        else: return f'The student has no grades.'
+    else:
+        return"Student not found."
+
+# # 3. Restaurant Menu Editor
+# def change_availability(): pass
+# def total_available_price(): pass
+dish_list:dict = {}
+def add_dish(dish:str,price:float,availability:bool)->None:
+    if dish in dish_list:
+        print("The dish already exists.")
+        return
+    dish_list[dish] = {"price":price,"availability":availability}
+def change_availability(dish:str)->None:
+    if dish in dish_list:
+        dish_list[dish]["availability"] = not dish_list[dish]["availability"]
+    else: print("Dish not found.")
+def show_books()->None:
+    if library:
+        print(f"{'Title':<20}{'Author':^20}{'Pages':>10}")
+        for title,book in library.items():
             print(f"{title.title():<20}{book['author'].title():^20}{book['pages']:>10}")
     else: print("No books found.")
 def book_menu()->None:
@@ -37,7 +85,7 @@ def book_menu()->None:
                 case 1:
                     add_book(validation('\nEnter the name of the book: ', lambda x: re.search(r'^[a-z\d\s]+$', x, re.IGNORECASE) ,str, ' Only letters, numbers and spaces between words are allowed'),
                               validation('\nEnter the author of the book: ', lambda x: re.search(r'^[a-z\d\s]+$', x, re.IGNORECASE) ,str, ' Only letters, numbers and spaces between words are allowed'),
-                              validation('\nEnter the number of pages the book has: ', lambda x: x >= 1,float, ' The number of pages must be greater than 0'))
+                              validation('\nEnter the number of pages the book has: ', lambda x: x >= 1,int, ' The number of pages must be greater than 0'))
                 case 2:
                     find_book(str(input('\nEnter the name of the book you want to search for: ')))
                 case 3: show_books()
@@ -46,16 +94,6 @@ def book_menu()->None:
         except ValueError: print('\nInvalid option.')
 #book_menu()
 
-#  2. Student Grade Manager
-# def add_student(): pass
-# def add_grade(): pass
-# def get_average(): pass
-#
-# # 3. Restaurant Menu Editor
-# def add_dish(): pass
-# def change_availability(): pass
-# def total_available_price(): pass
-#
 # # 4. Warehouse Box Counter
 # def add_box(): pass
 # def update_quantity(): pass
